703303194021c4ab135d0a1a02041f54979d8549
Nicholas Leask <leask.nicholas@gmail.com>
2018-09-05 19:02:24 -0400

    add production DB connection

--- knexfile.js
+++ knexfile.js
@@ -2,5 +2,8 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/rooms'
-  }
+  }, production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  },
 };
