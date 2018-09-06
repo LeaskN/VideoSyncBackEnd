7ececb3323d851629ca53eb077a6214ed77d180f
Nicholas Leask <leask.nicholas@gmail.com>
2018-09-06 16:07:40 -0400

    handling error response

--- api/videos.js
+++ api/videos.js
@@ -6,6 +6,9 @@ router.get('/search/:term', function(req, res, next) {
   fetch(`https://www.googleapis.com/youtube/v3/search?q=${req.params.term}&maxResults=50&type=video&part=snippet&key=${process.env.YOUTUBE_API_KEY}`)
     .then((res)=> res.json())
     .then(body => {
+      if(!body.items){
+        return [];
+      }
       const ids = body.items.map(item => item.id.videoId).join(',')
       fetch(`https://www.googleapis.com/youtube/v3/videos?id=${ids}&part=snippet,contentDetails&key=${process.env.YOUTUBE_API_KEY}`)
         .then((res)=> res.json())
